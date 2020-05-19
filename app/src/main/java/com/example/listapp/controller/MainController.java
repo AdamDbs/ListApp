206fee0135be35a468de0ffeb9aa6f971e5b672c
AdamDbs <addubois@et.esiea.fr>
2020-05-19 22:23:39 +0200

    Ajout de l'archi MVC pour l'ecran principal

---
+++ app/src/main/java/com/example/listapp/controller/MainController.java
@@ -0,0 +1,106 @@
+package com.example.listapp.controller;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import com.example.listapp.RMApi;
+import com.example.listapp.model.RestRMResponse;
+import com.example.listapp.model.RickandMorty;
+import com.example.listapp.view.MainActivity;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainController {
+
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
+    private MainActivity view;
+
+    public MainController(MainActivity mainActivity, Gson gson, SharedPreferences sharedPreferences) {
+        this.view = mainActivity;
+        this.gson = gson;
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    public void onStart(){
+
+
+        List<RickandMorty> rickandMortyList = getDataFromCache();
+
+        if(rickandMortyList != null){
+            view.showList(rickandMortyList);
+        }else {
+            makeApiCall();
+        }
+
+    }
+
+    private void makeApiCall(){
+
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MainActivity.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        RMApi rmapi = retrofit.create(RMApi.class);
+
+        Call<RestRMResponse> call = rmapi.getRMResponse();
+        call.enqueue(new Callback<RestRMResponse>() {
+            @Override
+            public void onResponse(Call<RestRMResponse> call, Response<RestRMResponse> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    List<RickandMorty> rickandMortyList = response.body().getResults();
+                    saveList(rickandMortyList);
+                    view.showList(rickandMortyList);
+                }else{
+                    view.showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestRMResponse> call, Throwable t) {
+                view.showError();
+            }
+        });
+
+
+    }
+
+    private void saveList(List<RickandMorty> rickandMortyList) {
+        String jsonString = gson.toJson(rickandMortyList);
+        sharedPreferences
+                .edit()
+                .putString("jsonrickandMortyList", jsonString)
+                .apply();
+
+    }
+
+    private List<RickandMorty> getDataFromCache(){
+        String jsonRickandMorty = sharedPreferences.getString("jsonrickandMortyList", null);
+
+        if(jsonRickandMorty == null){
+            return null;
+        }else{
+            Type listType = new TypeToken<List<RickandMorty>>(){}.getType();
+            return gson.fromJson(jsonRickandMorty, listType);
+        }
+
+    }
+
+    public void onItemClick(RickandMorty rickandMorty){
+
+    }
+}
--- app/src/main/java/com/example/listapp/view/MainActivity.java
+++ app/src/main/java/com/example/listapp/view/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.example.listapp.R;
 import com.example.listapp.RMApi;
+import com.example.listapp.controller.MainController;
 import com.example.listapp.model.RestRMResponse;
 import com.example.listapp.model.RickandMorty;
 import com.google.gson.Gson;
@@ -28,50 +29,32 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String BASE_URL = "https://rickandmortyapi.com/";
+    public static final String BASE_URL = "https://rickandmortyapi.com/";
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private SharedPreferences sharedPreferences;
-    private Gson gson;
 
+
+    private MainController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sharedPreferences = getSharedPreferences("application_esiea", Context.MODE_PRIVATE);
-
-        gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
-
-
-        List<RickandMorty> rickandMortyList = getDataFromCache();
-
-        if(rickandMortyList != null){
-            showList(rickandMortyList);
-        }else {
-            makeApiCall();
-        }
-
-    }
-    private List<RickandMorty> getDataFromCache(){
-        String jsonRickandMorty = sharedPreferences.getString("jsonrickandMortyList", null);
-
-        if(jsonRickandMorty == null){
-            return null;
-        }else{
-            Type listType = new TypeToken<List<RickandMorty>>(){}.getType();
-            return gson.fromJson(jsonRickandMorty, listType);
-        }
-
+        controller = new MainController(
+                this,
+                new GsonBuilder()
+                        .setLenient()
+                        .create(),
+            getSharedPreferences("application_esiea", Context.MODE_PRIVATE)
+        );
+        controller.onStart();
     }
 
-    private void showList(List<RickandMorty> rickandMortyList) {
+
+    public void showList(List<RickandMorty> rickandMortyList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
@@ -84,50 +67,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void makeApiCall(){
 
-
-
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-
-            RMApi rmapi = retrofit.create(RMApi.class);
-
-            Call<RestRMResponse> call = rmapi.getRMResponse();
-            call.enqueue(new Callback<RestRMResponse>() {
-                @Override
-                public void onResponse(Call<RestRMResponse> call, Response<RestRMResponse> response) {
-                    if(response.isSuccessful() && response.body() != null){
-                            List<RickandMorty> rickandMortyList = response.body().getResults();
-                            saveList(rickandMortyList);
-                            showList(rickandMortyList);
-                    }else{
-                        showError();
-                    }
-                }
-
-                @Override
-                public void onFailure(Call<RestRMResponse> call, Throwable t) {
-                    showError();
-                }
-            });
-
-
-    }
-
-    private void saveList(List<RickandMorty> rickandMortyList) {
-        String jsonString = gson.toJson(rickandMortyList);
-        sharedPreferences
-                .edit()
-                .putString("jsonrickandMortyList", jsonString)
-                .apply();
-
-        Toast.makeText(this, "List Saved", Toast.LENGTH_SHORT).show();
-    }
-
-    private void showError() {
+    public void showError() {
         Toast.makeText(this, "API Error", Toast.LENGTH_SHORT).show();
     }
 }
